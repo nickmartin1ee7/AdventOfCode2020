8aa010beb909fd91f7b135db112d7fc83862ee62
Nicholas M Martin <nmmart04@louisville.edu>
2020-12-08 12:20:10 -0500

    Scaffolded day 7

---
+++ AdventOfCode.Library/Day7/Day7.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode.Library.Day7
+{
+    public class Day7 : BaseSolution
+    {
+        private string _rawData;
+
+        public Day7()
+        {
+            _rawData = ReadAllText(GetType());
+        }
+
+        public string SilverStar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GoldStar()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode.Tests/Day7Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Library;
+using AdventOfCode.Library.Day7;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    public class Day7Tests
+    {
+        private ISolution _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new Day7();
+        }
+
+        [TestCase("")]
+        public void SilverStarTest(string expected)
+        {
+            Assert.AreEqual(expected, _solution.SilverStar());
+        }
+
+        [TestCase("")]
+        public void GoldStarTest(string expected)
+        {
+            Assert.AreEqual(expected, _solution.GoldStar());
+        }
+    }
+}
