c449d71909c7b11735a0582648fa35267ad7c496
Nicholas M Martin <nmmart04@louisville.edu>
2020-12-08 12:15:01 -0500

    Implemented Day 6

---
+++ AdventOfCode.Library/Day6/Day6.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Library.Day6
+{
+    public class Day6 : BaseSolution
+    {
+        private string _rawData;
+
+        public Day6()
+        {
+            _rawData = ReadAllText(GetType());
+        }
+
+        public override string SilverStar()
+        {
+            var groupsData = _rawData.Split(Environment.NewLine + Environment.NewLine);
+            int total = 0;
+            foreach (var groupData in groupsData)
+            {
+                total += new Group(groupData.Trim().Split(Environment.NewLine)).Answers.Keys.Count;
+            }
+
+            return $"{total}";
+        }
+
+        public override string GoldStar()
+        {
+            var groupsData = _rawData.Split(Environment.NewLine + Environment.NewLine);
+            int total = 0;
+            foreach (var groupData in groupsData)
+            {
+                var group = new Group(groupData.Trim().Split(Environment.NewLine));
+                total += group.InAgreementQuestions;
+            }
+
+            return $"{total}";
+        }
+    }
+
+    public class Group
+    {
+        private readonly string[] _data;
+
+        public Dictionary<char, List<bool>> Answers { get; } = new Dictionary<char, List<bool>>();
+        
+        public int InAgreementQuestions
+        {
+            get => Answers.Values.Count(answers => answers.Count(a => a) == _data.Length);
+        }
+
+        public Group(string[] data)
+        {
+            _data = data;
+
+            foreach (var person in _data)
+            {
+                foreach (var question in person)
+                {
+                    if (Answers.TryGetValue(question, out List<bool> answers))
+                    {
+                        answers.Add(true);
+                        Answers.TryAdd(question, answers);
+                    }
+                    else
+                    {
+                        Answers.TryAdd(question, new List<bool> { true });
+                    }
+                }
+            }
+        }
+    }   
+}
---
+++ AdventOfCode.Tests/Day6Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Library;
+using AdventOfCode.Library.Day6;
+using NUnit.Framework;
+using Group = AdventOfCode.Library.Day6.Group;
+
+namespace AdventOfCode.Tests
+{
+    public class Day6Tests
+    {
+        private ISolution _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new Day6();
+        }
+
+        [TestCase("6530")]
+        public void SilverStarTest(string expected)
+        {
+            Assert.AreEqual(expected, _solution.SilverStar());
+        }
+
+        [TestCase("3323")]
+        public void GoldStarTest(string expected)
+        {
+            Assert.AreEqual(expected, _solution.GoldStar());
+        }
+
+        [TestCase("11", "abc\r\n\r\na\r\nb\r\nc\r\n\r\nab\r\nac\r\n\r\na\r\na\r\na\r\na\r\n\r\nb")]
+        public void GroupAnyAnswersTest(string expected, string data)
+        {
+            var groupsData = data.Split(Environment.NewLine + Environment.NewLine);
+            int total = 0;
+            foreach (var groupData in groupsData)
+            {
+                total += new Group(groupData.Trim().Split(Environment.NewLine)).Answers.Keys.Count;
+            }
+            Assert.AreEqual(expected, $"{total}");
+        }
+
+        [TestCase("6", "abc\r\n\r\na\r\nb\r\nc\r\n\r\nab\r\nac\r\n\r\na\r\na\r\na\r\na\r\n\r\nb")]
+        public void GroupInAgreementQuestionsTest(string expected, string data)
+        {
+            var groupsData = data.Split(Environment.NewLine + Environment.NewLine);
+            int total = 0;
+            foreach (var groupData in groupsData)
+            {
+                var group = new Group(groupData.Trim().Split(Environment.NewLine));
+                total += group.InAgreementQuestions;
+            }
+
+            Assert.AreEqual(expected, $"{total}");
+        }
+    }
+}
